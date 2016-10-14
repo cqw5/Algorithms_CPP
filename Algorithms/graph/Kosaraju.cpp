89e486eb6b8bbabed287f9e28723b2cfb3798e6f
cqw5 <1040688029@qq.com>
2016-10-14 22:34:14 +0800

    add LPTSchedule

--- Algorithms/graph/Kosaraju.cpp
+++ Algorithms/graph/Kosaraju.cpp
@@ -1,7 +1,7 @@
 /*!
  * 计算有向图无权图的强连通分量的Kosaraju算法
  * 思路：1.给定一幅图G，先计算它的反向图的逆后序排列
- *		 2.在G中进行标准的深度优先搜索，但是按照刚才计算得到的顺序而非标准的顺序来访问所有为被标记的结点
+ *       2.在G中进行标准的深度优先搜索，但是按照刚才计算得到的顺序而非标准的顺序来访问所有为被标记的结点
  */
 
 #include <iostream>
@@ -12,93 +12,98 @@ using namespace std;
 
 class Graph {
 public:
-	Graph(int num_v);
-	void addEdge(int v, int w);
-	void dfs(int v, bool marked[]);	// 深度优先
-	Graph getReverse();				// 获取图的反向图
-	void getReversePost(Graph &G, int v, bool marked[], stack<int> &reverse_post_stack); // 获取图的逆后序排列
-	void printStronglyConnectedComponent();	// 输出图的强连通子图
+    Graph(int num_v);
+    void addEdge(int v, int w);
+    void dfs(int v, bool marked[]);	// 深度优先
+    Graph getReverse();				// 获取图的反向图
+    void getReversePost(Graph &G, int v, bool marked[], stack<int> &reverse_post_stack); // 获取图的逆后序排列
+    void printStronglyConnectedComponent();	// 输出图的强连通子图
 private:
-	int num_v_;			// 图的结点数
-	vector<int> *adj_;	// 图的邻接数组
+    int num_v_;			// 图的结点数
+    vector<int> *adj_;	// 图的邻接数组
 };
 
 Graph::Graph(int num_v) {
-	num_v_ = num_v;
-	adj_ = new vector<int>[num_v];
+    num_v_ = num_v;
+    adj_ = new vector<int>[num_v];
 }
 
 void Graph::addEdge(int v, int w) {
-	adj_[v].push_back(w);
+    adj_[v].push_back(w);
 }
 
 void Graph::dfs(int v, bool marked[]) {
-	marked[v] = true;
+    marked[v] = true;
 	cout << v << " ";
-	for (auto w = adj_[v].begin(); w != adj_[v].end(); ++w) {
-		if (!marked[*w]) {
-			dfs(*w, marked);
-		}
-	}
+    for (auto w = adj_[v].begin(); w != adj_[v].end(); ++w) {
+        if (!marked[*w]) {
+            dfs(*w, marked);
+        }
+    }
 }
 
 Graph Graph::getReverse() {
-	Graph reverse_G(num_v_);
-	for (int v = 0; v < num_v_; v++) {
-		for (auto w = adj_[v].begin(); w != adj_[v].end(); ++w) {
-			reverse_G.adj_[*w].push_back(v);
-		}
-	}
-	return reverse_G;
+    Graph reverse_G(num_v_);
+    for (int v = 0; v < num_v_; v++) {
+        for (auto w = adj_[v].begin(); w != adj_[v].end(); ++w) {
+            reverse_G.adj_[*w].push_back(v);
+        }
+    }
+    return reverse_G;
 }
 
 void Graph::getReversePost(Graph &G, int v, bool marked[], stack<int> &reverse_post_stack) {
-	marked[v] = true;
-	for (auto w = G.adj_[v].begin(); w != G.adj_[v].end(); ++w) {
-		if (!marked[*w]) {
-			getReversePost(G, *w, marked, reverse_post_stack);
-		}
-	}
-	reverse_post_stack.push(v); // 逆后序：在递归调用之后将顶点压入栈
+    marked[v] = true;
+    for (auto w = G.adj_[v].begin(); w != G.adj_[v].end(); ++w) {
+        if (!marked[*w]) {
+            getReversePost(G, *w, marked, reverse_post_stack);
+        }
+    }
+    reverse_post_stack.push(v); // 逆后序：在递归调用之后将顶点压入栈
 }
 
 void Graph::printStronglyConnectedComponent() {
-	// marked用来标记已访问过的结点，初始值为false，访问过为true
-	bool *marked = new bool[num_v_];
-	for (int i = 0; i < num_v_; i++) {
-		marked[i] = false;
-	}
+    // marked用来标记已访问过的结点，初始值为false，访问过为true
+    bool *marked = new bool[num_v_];
+    for (int i = 0; i < num_v_; i++) {
+        marked[i] = false;
+    }
 	// 获取图G的反向图reverse_G
-	Graph reverse_G = getReverse();
-	// 后续出栈的顺序就是逆后序遍历的顺序
-	stack<int> reverse_post_stack;
-	for (int v = 0; v < num_v_; ++v) {
-		if (!marked[v]) {
-			getReversePost(reverse_G, v, marked, reverse_post_stack);
-		}
-	}
-	for (int i = 0; i < num_v_; i++) {
-		marked[i] = false;
-	}
-	// 按照反向图reverse_G的逆后序遍历的顺序对图G进行DFS
-	while (!reverse_post_stack.empty()) {
-		int v = reverse_post_stack.top();
-		reverse_post_stack.pop();
-		if (!marked[v]) {
-			dfs(v, marked);
-			cout << endl;
-		}
-	}
+    Graph reverse_G = getReverse();
+    // 后续出栈的顺序就是逆后序遍历的顺序
+    stack<int> reverse_post_stack;
+    for (int v = 0; v < num_v_; ++v) {
+        if (!marked[v]) {
+            getReversePost(reverse_G, v, marked, reverse_post_stack);
+        }
+    }
+    for (int i = 0; i < num_v_; i++) {
+    	marked[i] = false;
+    }
+    // 按照反向图reverse_G的逆后序遍历的顺序对图G进行DFS
+    while (!reverse_post_stack.empty()) {
+        int v = reverse_post_stack.top();
+        reverse_post_stack.pop();
+        if (!marked[v]) {
+            dfs(v, marked);
+            cout << endl;
+        }
+    }
 }
 
 // 测试
 void testKosaraju() {
-	Graph G(5);
-	G.addEdge(1, 0);
-	G.addEdge(0, 2);
-	G.addEdge(2, 1);
-	G.addEdge(0, 3);
-	G.addEdge(3, 4);
-	cout << "The strongly connected components in given graph" << endl;
-	G.printStronglyConnectedComponent();
+    Graph G(5);
+    G.addEdge(1, 0);
+    G.addEdge(0, 2);
+    G.addEdge(2, 1);
+    G.addEdge(0, 3);
+    G.addEdge(3, 4);
+    cout << "The strongly connected components in given graph" << endl;
+    G.printStronglyConnectedComponent();
+}
+
+int main(){
+	testKosaraju();
+	return 0;
 }
---
+++ Algorithms/greedy_method/LPTSchedule.cpp
@@ -0,0 +1,103 @@
+/*！
+ * 机器调度问题：
+ * 一个工厂有m台一模一样的机器，有n个任务要处理。
+ * * 一台机器在同一个时间内只能处理一个作业
+ * * 一个作业不能同时在两台机器上处理
+ * 求最少的调度时间？
+ * 这是一个NPC问题，采用近似算法来解决。
+ * 思想：采用LPT算法，将作业按处理时间递减顺序排序。当一个作业需要分配时，总是分配给最先变为空闲的机器。
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class jobNode {
+public:
+    jobNode(){}
+    jobNode(int id, int time){
+    	id_ = id;
+    	time_ = time;
+    }
+    int getId(){
+    	return id_;
+    }
+    int getTime(){
+    	return time_;
+    }
+    operator int() const { // 定义通过time来比较jobNode的大小
+    	return time_;
+    }
+private:
+    int id_,    // 执行任务的ID
+        time_;  // 执行任务所需要的时间
+};
+
+class machineNode {
+public:
+	machineNode(){}
+    machineNode(int id, int avail){
+        id_ = id;
+        avail_ = avail;
+    }
+    int getId(){
+    	return id_;
+    }
+    int getAvail(){
+    	return avail_;
+    }
+    void setAvail(int avail){
+        avail_ = avail;	
+    }
+    operator int() const {
+    	return avail_;
+    }
+private:
+    int id_,      // 机器ID
+        avail_;   // 当前机器开始的空闲时间
+};
+
+void makeSchedule(vector<jobNode> job, vector<machineNode> machine, int num_job, int num_machine){
+    // 如果机器数大于等于任务数，直接分配即可
+    if (num_machine >= num_job){
+    	cout << "Schedule each job on a different machine." << endl;
+    	return;
+    }
+    // 先对任务从大到小排序
+    sort(job.begin(), job.end(), greater<int>());
+    // 对机器列表构建小顶堆
+    make_heap(machine.begin(), machine.end(), greater<int>());
+    for (int i = 0; i < num_job; i++){
+    	// 每次总是从堆中去avail最小的机器来分配任务
+        machineNode avail_machine = machine.front();
+        pop_heap(machine.begin(), machine.end(), greater<int>()); machine.pop_back();
+        cout << "Schedule job " << job[i].getId() << " on machine " << avail_machine.getId()
+            << " from " << avail_machine.getAvail() 
+            << " to " << (avail_machine.getAvail() + job[i].getTime()) 
+            << endl;
+        // 将机器的avail加上新的任务的运行时间，重新放入堆中
+        avail_machine.setAvail(avail_machine.getAvail() + job[i].getTime());
+        machine.push_back(avail_machine); push_heap(machine.begin(), machine.end(), greater<int>());
+    }
+}
+
+void testMakeSchedule(){
+    int num_machine = 3;
+    int num_job = 10;
+    vector<machineNode> machine;
+    vector<jobNode> job;
+    for (int i = 0; i < num_machine; i++){
+        machine.push_back(machineNode(i, 0));
+    }
+	for (int i = 0; i < num_job; i++){
+        job.push_back(jobNode(i, 2 * i * i));
+    }
+    makeSchedule(job, machine, num_job, num_machine);
+}
+
+int main(){
+    testMakeSchedule();
+    return 0;
+}
